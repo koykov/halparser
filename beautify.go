5f78a35f7412cb90ddfd1d2d04ac3add1d6dca45
Konstantin Voykov <koykov0@gmail.com>
2022-07-19 18:28:04 +0300

    Simple language (no script or region) without quality.

--- beautify.go
+++ beautify.go
@@ -6,6 +6,84 @@ import (
 	"github.com/koykov/vector"
 )
 
+var (
+	// Byte constants.
+	btSpace  = []byte(` `)
+	btQuote  = []byte(`"`)
+	btComma  = []byte(`,`)
+	btDotDot = []byte(`:`)
+	btNl     = []byte("\n")
+	btTab    = []byte("\t")
+	btArrO   = []byte(`[`)
+	btArrC   = []byte(`]`)
+	btArrE   = []byte(`[]`)
+	btObjO   = []byte(`{`)
+	btObjC   = []byte(`}`)
+	btObjE   = []byte(`{}`)
+	btNull   = []byte(`null`)
+)
+
+// Internal beautifier helper.
+//
+// Writes beautified JSON to w.
 func (vec *Vector) beautify(w io.Writer, node *vector.Node, depth int) (err error) {
+	switch node.Type() {
+	case vector.TypeNull:
+		_, err = w.Write(btNull)
+	case vector.TypeNum, vector.TypeBool:
+		_, err = w.Write(node.ForceBytes())
+	case vector.TypeStr:
+		_, err = w.Write(btQuote)
+		_, err = w.Write(node.RawBytes())
+		_, err = w.Write(btQuote)
+	case vector.TypeArr:
+		if node.Limit() == 0 {
+			_, err = w.Write(btArrE)
+		} else {
+			_, err = w.Write(btArrO)
+			_, err = w.Write(btNl)
+			node.Each(func(idx int, node *vector.Node) {
+				if idx > 0 {
+					_, err = w.Write(btComma)
+					_, err = w.Write(btNl)
+				}
+				writePad(w, node.Depth())
+				err = vec.beautify(w, node, depth+1)
+			})
+			_, err = w.Write(btNl)
+			writePad(w, node.Depth())
+			_, err = w.Write(btArrC)
+		}
+	case vector.TypeObj:
+		if node.Limit() == 0 {
+			_, err = w.Write(btObjE)
+		} else {
+			_, err = w.Write(btObjO)
+			_, err = w.Write(btNl)
+			node.Each(func(idx int, node *vector.Node) {
+				if idx > 0 {
+					_, err = w.Write(btComma)
+					_, err = w.Write(btNl)
+				}
+				writePad(w, node.Depth())
+				_, err = w.Write(btQuote)
+				_, err = w.Write(node.KeyBytes())
+				_, err = w.Write(btQuote)
+				_, err = w.Write(btDotDot)
+				_, err = w.Write(btSpace)
+				err = vec.beautify(w, node, depth+1)
+			})
+			_, err = w.Write(btNl)
+			writePad(w, node.Depth())
+			_, err = w.Write(btObjC)
+		}
+	}
 	return
+}
+
+// Write number of tabs to w.
+func writePad(w io.Writer, cnt int) {
+	for i := 0; i < cnt; i++ {
+		_, _ = w.Write(btTab)
+	}
 }
--- parser.go
+++ parser.go
@@ -1,13 +1,35 @@
 package halvector
 
 import (
+	"errors"
+
 	"github.com/koykov/bytealg"
 	"github.com/koykov/vector"
 )
 
+const (
+	offsetCode    = 0
+	offsetScript  = 4
+	offsetRegion  = 10
+	offsetQuality = 16
+	offsetDefQT   = 23
+
+	lenCode    = 4
+	lenScript  = 6
+	lenRegion  = 6
+	lenQuality = 7
+	lenDefQT   = 3
+)
+
 var (
 	// Byte constants.
-	bFmt = []byte(" \t\n\r")
+	bFmt   = []byte(" \t")
+	bQt    = []byte(";q=")
+	bComma = []byte(",")
+	bSep   = []byte("-,")
+	bKV    = []byte("codescriptregionquality1.0")
+
+	ErrTooManyParts = errors.New("entry contains too many parts")
 )
 
 func (vec *Vector) parse(s []byte, copy bool) (err error) {
@@ -18,10 +40,11 @@ func (vec *Vector) parse(s []byte, copy bool) (err error) {
 
 	offset := 0
 	// Create root node and register it.
-	root, i := vec.GetNode(0)
+	root, i := vec.GetNodeWT(0, vector.TypeArr)
+	root.SetOffset(vec.Index.Len(1))
 
 	// Parse source data.
-	offset, err = vec.parseGeneric(0, offset, root)
+	offset, err = vec.parseGeneric(1, offset, root)
 	if err != nil {
 		vec.SetErrOffset(offset)
 		return err
@@ -38,5 +61,108 @@ func (vec *Vector) parse(s []byte, copy bool) (err error) {
 }
 
 func (vec *Vector) parseGeneric(depth, offset int, node *vector.Node) (int, error) {
+	var (
+		err error
+		eof bool
+	)
+	for offset < vec.SrcLen() {
+		if offset, eof = vec.skipFmt(offset); eof {
+			return offset, vector.ErrUnexpEOF
+		}
+		var qlo, qhi int
+		if qlo = bytealg.IndexAt(vec.Src(), bQt, offset); qlo == -1 {
+			qlo = vec.SrcLen()
+		} else if qhi = bytealg.IndexAt(vec.Src(), bComma, qlo+3); qhi == -1 {
+			qhi = vec.SrcLen()
+		}
+		if offset, err = vec.parseNode(depth, offset, qlo, qhi, node); err != nil {
+			return offset, err
+		}
+		if offset, eof = vec.skipFmt(offset); eof {
+			return offset, nil
+		}
+	}
 	return offset, nil
+}
+
+func (vec *Vector) parseNode(depth, offset int, qlo, qhi int, root *vector.Node) (int, error) {
+	for {
+		node, i := vec.GetChildWT(root, depth, vector.TypeObj)
+		node.SetOffset(vec.Index.Len(depth + 1))
+		p := bytealg.IndexAnyAt(vec.Src(), bSep, offset)
+		if p == -1 {
+			p = vec.SrcLen()
+		}
+		a, b, ok := vec.indexDash(offset, p)
+		if !ok {
+			return offset, ErrTooManyParts
+		}
+		cl := qlo
+		if a != 0 {
+			cl = a
+		}
+		child, j := vec.GetChildWT(node, depth+1, vector.TypeStr)
+		child.Key().Init(bKV, offsetCode, lenCode)
+		child.Value().Init(vec.Src(), offset, cl)
+		vec.PutNode(j, child)
+		offset = cl + 1
+
+		_, _ = a, b
+
+		// if b > 0 {
+		// 	child, j := vec.GetChildWT(node, depth+1, vector.TypeStr)
+		// 	child.Key().Init(bKV, offsetScript, offsetScript+lenScript)
+		// 	child.Value().Init(vec.Src(), offset, b)
+		// 	vec.PutNode(j, child)
+		// 	offset = b + 1
+		// }
+
+		child, j = vec.GetChildWT(node, depth+1, vector.TypeNum)
+		child.Key().Init(bKV, offsetQuality, lenQuality)
+		if qlo > 0 && qhi > qlo {
+			child.Value().Init(vec.Src(), qlo, qhi)
+		} else {
+			child.Value().Init(bKV, offsetDefQT, lenDefQT)
+		}
+		vec.PutNode(j, child)
+
+		vec.PutNode(i, node)
+
+		break
+	}
+	return offset, nil
+}
+
+func (vec *Vector) skipFmt(offset int) (int, bool) {
+loop:
+	if offset >= vec.SrcLen() {
+		return offset, true
+	}
+	if c := vec.SrcAt(offset); c != bFmt[0] && c != bFmt[1] {
+		return offset, false
+	}
+	offset++
+	goto loop
+}
+
+func (vec *Vector) indexDash(lo, hi int) (a, b int, ok bool) {
+	var c int
+	ok = true
+loop:
+	if vec.SrcAt(lo) == '-' {
+		if c == 0 {
+			a = lo
+		} else if c == 1 {
+			b = lo
+		} else {
+			ok = false
+			return
+		}
+		c++
+	}
+	lo++
+	if lo == hi {
+		return
+	}
+	goto loop
 }
